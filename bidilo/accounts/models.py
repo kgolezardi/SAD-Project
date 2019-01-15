c6825241db242907348ea9c8cdf28efe43e1def4
Kiarash Golezardi <golezardi@ce.sharif.edu>
2019-01-15 13:19:39 +0330

    Use popover for profile in navbar

--- bidilo/accounts/models.py
+++ bidilo/accounts/models.py
@@ -5,5 +5,6 @@ from django.db import models
 class User(AbstractUser):
     # add additional fields in here
     credit = models.PositiveIntegerField(default=0)
+
     def __str__(self):
         return self.username
