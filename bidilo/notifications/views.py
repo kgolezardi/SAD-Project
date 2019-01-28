63ba50fd0dc79103b47b7e92ac36b968b2906331
Kiarash Golezardi <golezardi@ce.sharif.edu>
2019-01-28 15:44:55 +0330

    Use Ajax to read/unread notifications

--- bidilo/notifications/views.py
+++ bidilo/notifications/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.http import HttpResponseForbidden
 from django.http import HttpResponseRedirect
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
@@ -14,17 +15,22 @@ def show_notifs(request):
     return render(request, 'notifications/show_all.html', context={"notifs": notifs})
 
 
-def read_notif(request, notif_id):
+def read_notif(request):
+    notif_id = request.POST.get('notif_id', -1)
     notification = get_object_or_404(Notification, id=notif_id)
+    print(request.user)
     if notification.user != request.user:
         return HttpResponseForbidden()
     notification.set_read()
-    return HttpResponseRedirect(reverse('notifications:show_notifs'))
+    data = {'done': True}
+    return JsonResponse(data)
 
 
-def unread_notif(request, notif_id):
+def unread_notif(request):
+    notif_id = request.POST.get('notif_id', -1)
     notification = get_object_or_404(Notification, id=notif_id)
     if notification.user != request.user:
         return HttpResponseForbidden()
     notification.set_unread()
-    return HttpResponseRedirect(reverse('notifications:show_notifs'))
+    data = {'done': True}
+    return JsonResponse(data)
